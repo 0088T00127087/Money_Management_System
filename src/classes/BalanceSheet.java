d29a164ddbe28b52f85bc0d39421c04ab566e133
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:46:47 +0000

    Added a constructor to BalanceSheet class

--- src/classes/BalanceSheet.java
+++ src/classes/BalanceSheet.java
@@ -3,6 +3,20 @@ package classes;
 
 public class BalanceSheet {
 
+    String accNumber;
+    Income income;
+    Expenditure expenditure;
+    double calculation;
+    String frequency;
+
+    public BalanceSheet(String accNumber){
+        this.accNumber = "";
+        income = new Income();
+        expenditure = new Expenditure();
+        calculation = 0;
+        frequency = "";
+    }
+
 
 
 }
