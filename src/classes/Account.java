5803c6f99c9ed2e7344290a7ad5e92fa77f7ec95
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:33:46 +0000

    Changed class name Balance to BalanceSheet

--- src/classes/Account.java
+++ src/classes/Account.java
@@ -1,35 +1,11 @@
 package classes;
 
 
-public class Account extends Expenditure  {
-
-
-    String accountNumber;
-
-    public Account(){
-
-        String accountNumber;
-
-        Expenditure exp = new Expenditure();
-        Income  income = new Income();
-        Object Balance = new BalanceSheet();
-    }
+public class Account  {
 
 
 
 
-    public void setAccountNumber(){
 
-        for(int i = 0; i <5; i++){
-         double n = Math.random();
-
-        }
-    }
-
-    public String getAccountNumber(){
-        return accountNumber;
-    }
 
 }
-
-
