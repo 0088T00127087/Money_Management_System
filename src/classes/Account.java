c433fc6beb0fc7d19a075242d61e71c5ea55c115
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:38:51 +0000

    added getter and setter for account number in account class.

--- src/classes/Account.java
+++ src/classes/Account.java
@@ -3,8 +3,18 @@ package classes;
 
 public class Account  {
 
+    int accNumber;
+    User user = new User();
+    BalanceSheet blSheet = new BalanceSheet();
 
 
+    public void setAccNumber(int accNumber){
+        this.accNumber = accNumber;
+    }
+
+    public int getAccNumber(){
+        return accNumber;
+    }
 
 
 
