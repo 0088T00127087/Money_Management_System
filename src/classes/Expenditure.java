ad6c51718f1bf6ed7321a3c65fe0c4cef94ce0bc
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:49:52 +0000

    Added class variables and a constructor and getter and setter methods for Expenditure class

--- src/classes/Expenditure.java
+++ src/classes/Expenditure.java
@@ -2,5 +2,38 @@ package classes;
 
 public class Expenditure {
 
+    String ExpenType;
+    double amount;
+    String frequency;
 
+    public Expenditure(String expenType, double amount, String frequency) {
+        ExpenType = expenType;
+        this.amount = amount;
+        this.frequency = frequency;
+
+    }
+
+    public String getExpenType() {
+        return ExpenType;
+    }
+
+    public void setExpenType(String expenType) {
+        ExpenType = expenType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
 }
