4ccf0cd4bf4e2f03bb4fa25cea2415f10a8d155c
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:47:28 +0000

    Cleaned all previous code from Expenditure class

--- src/classes/Expenditure.java
+++ src/classes/Expenditure.java
@@ -2,31 +2,5 @@ package classes;
 
 public class Expenditure {
 
-    String typeOfExpenditure;
-    int cost;
-    String date;
 
-    public String getTypeOfExpenditure() {
-        return typeOfExpenditure;
-    }
-
-    public void setTypeOfExpenditure(String typeOfExpenditure) {
-        this.typeOfExpenditure = typeOfExpenditure;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 }
