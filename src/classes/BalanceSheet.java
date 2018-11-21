f9cbc2605a1a172f3b867042cf9cf9bf742ed7ff
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-21 23:42:33 +0000

    cleaned out BalanceSheet class of all previous code.

--- src/classes/BalanceSheet.java
+++ src/classes/BalanceSheet.java
@@ -1,22 +1,8 @@
 package classes;
 
 
-
 public class BalanceSheet {
 
-    int balance;
-
-    public BalanceSheet(){
-
-    }
 
 
-    public void set_balance(int bal) {
-        this.balance = bal;
-    }
-
-
-    public int get_balance() {
-        return this.balance;
-    }
 }
