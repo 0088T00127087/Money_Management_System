b5cdbeeb83197a52c0226e0033dd36b766e4a1ea
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-30 00:32:01 +0000

    Created Class Credit and implemented methods from Item interface

---
+++ src/concreteClasses/Credit.java
@@ -0,0 +1,37 @@
+package concreteClasses;
+import interfaces.Item;
+
+public class Credit implements Item {
+
+    protected String creditType = "";
+    protected int creditAmount = 0;
+    protected String creditFrequency = "";
+
+    public void display(){
+        System.out.println(creditType);
+        System.out.println(creditAmount);
+        System.out.println(creditFrequency);
+    }
+
+
+    public void setType(String type){
+        this.creditType = type;
+    }
+
+
+    public void setAmount(int amount){
+        this.creditAmount = amount;
+
+    }
+
+    public void setFrequency(String frequency){
+        this.creditFrequency = frequency;
+
+    }
+
+    public String toString(){
+        return "Type: " + creditType + "\nAmount: " + creditAmount + "\nFrequency: " + creditFrequency;
+    }
+
+
+}
