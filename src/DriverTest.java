4cb69021d8128dc201a193ed5122105b23904c28
joe Enright <Joseph.Enright@students.ittralee.ie>
2018-11-30 00:37:55 +0000

    Added code to test decorator pattern in DriverTest

--- src/DriverTest.java
+++ src/DriverTest.java
@@ -1,7 +1,19 @@
+import DecoratorClasses.*;
+import concreteClasses.*;
+import interfaces.Item;
+
 public class DriverTest  {
     public static void main (String [] args)  {
 
+        Item job = new ItemTypeDecorator(new Credit());
 
+        job.setType("Job");
+        job.setAmount(2500);
+        job.setFrequency("Monthly");
+
+        job.display();
+
+        job.toString();
 
 
 }
